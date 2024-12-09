36f7f8e7297a91c23587a58d4ba850ce631be3d8
bpn <bipinlamsal2004@gmail.com>
2024-12-09 23:53:21 +0530

    image paste

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Goals
-- make a note taking site
-- should be minimalistic
-- should support markdown formating
-- should be able to open file from pc (local)
-- should be able to save file to pc
-- can set custom colors
+- [x] make a note taking site
+- [x] should be minimalistic
+- [ ] should support markdown formating
+- [x] should support pasting image from clipboard
+- [ ] should be able to open file from pc (local)
+- [ ] should be able to save file to pc
+- [ ] can set custom colors
 > currently using [color palette](https://colorhunt.co/palette/181c143c3d37697565ecdfcc)
-- should work something like [this](https://stackedit.io/app)
+- [ ] should work something like [this](https://stackedit.io/app)
--- src/Body.jsx
+++ src/Body.jsx
@@ -21,6 +21,21 @@ function Body() {
         setLines(newLines);
     };
 
+    const handlePaste = (event) => {
+        const items = event.clipboardData.items;
+        for (let item of items) {
+            if (item.type.startsWith('image/')) {
+                const blob = item.getAsFile();
+                const imageUrl = URL.createObjectURL(blob);
+                const newLines = [...lines];
+                newLines[editingIndex] += ` ![](${imageUrl})`;
+                setLines(newLines);
+                event.preventDefault();
+                break;
+            }
+        }
+    };
+
     const containerCSS = {
         margin: "10px",
         padding: "10px",
@@ -33,7 +48,8 @@ function Body() {
 
     const lineCSS = {
         color: colors.foreground,
-        margin: "0px"
+        margin: "0px",
+        cursor: "text"
     };
 
     const inputCSS = {
@@ -54,48 +70,69 @@ function Body() {
     }, [editingIndex]);
 
     return (
-        <div id="container" style={containerCSS}>
-            <div>
-                {lines.map((line, index) => (
-                    <div
-                        key={index}
-                        onClick={() => handleLineClick(index)}
-                        style={lineCSS}
-                    >
-                        {editingIndex === index ? (
-                            <input
-                                ref={(el) => inputRefs.current[index] = el}
-                                value={line}
-                                onChange={(e) => handleEditChange(e, index)}
-                                onKeyDown={(e) => {
-                                    if (e.key === 'Enter') {
-                                        console.log('Total lines = ' + lines.length)
+        <div
+            id="container"
+            style={containerCSS}
+            onPaste={handlePaste}
+        >
+            {lines.map((line, index) => (
+                <div
+                    key={index}
+                    onClick={() => handleLineClick(index)}
+                    style={lineCSS}
+                >
+                    {editingIndex === index ? (
+                        <input
+                            ref={(el) => inputRefs.current[index] = el}
+                            value={line}
+                            onChange={(e) => handleEditChange(e, index)}
+                            onBlur={(e) => { setEditingIndex(null) }}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    const newLines = [...lines];
+                                    newLines.splice(editingIndex + 1, 0, '');
+                                    setLines(newLines);
+                                    setEditingIndex(editingIndex + 1);
+                                    e.preventDefault();
+                                }
+                                else if (e.key === 'Backspace') {
+                                    if (editingIndex > 0 && e.target.selectionStart == 0) {
                                         const newLines = [...lines];
-                                        newLines.splice(editingIndex + 1, 0, '');
+                                        newLines[editingIndex - 1] += newLines[editingIndex];
+                                        newLines.splice(editingIndex, 1);
                                         setLines(newLines);
+                                        setEditingIndex(editingIndex - 1);
+                                        e.preventDefault();
+                                    }
+                                }
+                                else if (e.key === 'ArrowDown') {
+                                    if (editingIndex < lines.length - 1) {
                                         setEditingIndex(editingIndex + 1);
                                         e.preventDefault();
                                     }
-                                    else if (e.key === 'Backspace' && editingIndex > 0 && !lines[editingIndex]) {
+                                }
+                                else if (e.key === 'ArrowUp') {
+                                    if (editingIndex > 0) {
                                         setEditingIndex(editingIndex - 1);
+                                        e.preventDefault();
                                     }
+                                }
+                            }}
+                            style={inputCSS}
+                        />
+                    ) : (
+                        line.trim() ? (
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: marked(line),
                                 }}
-                                style={inputCSS}
                             />
                         ) : (
-                            line.trim() ? (
-                                <div
-                                    dangerouslySetInnerHTML={{
-                                        __html: marked(line),
-                                    }}
-                                />
-                            ) : (
-                                <div style={{ height: "20.8px", margin: "16px 0px" }} />
-                            )
-                        )}
-                    </div>
-                ))}
-            </div>
+                            <div style={{ height: "20.8px", margin: "16px 0px" }} />
+                        )
+                    )}
+                </div>
+            ))}
         </div>
     );
 }
